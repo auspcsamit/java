06ede80b17ce6014d42523ea1f4aa76e6e91e76d
auspscamit <amitmadanbkn@gmail.com>
2022-11-09 22:07:23 +0530

    Sum of Array Except Self

---
+++ Sum of Array Except Self/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int a : arr) {
+                sum = sum + a;
+            }
+
+            sum = sum - arr[i];
+            System.out.print(sum + " ");
+
+        }
+    }
+}
