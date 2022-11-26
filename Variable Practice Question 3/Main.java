2592c6f56e8bf1e03c385d5fd6da1f1664544774
auspscamit <amitmadanbkn@gmail.com>
2022-11-27 00:27:23 +0530

    Variable Practice Question 3

---
+++ Variable Practice Question 3/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int c = a + b;
+
+        System.out.print(c);
+    }
+}
