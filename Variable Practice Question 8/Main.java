32b7343cc4183885396dbef811736409628d690a
auspscamit <amitmadanbkn@gmail.com>
2022-11-27 00:50:38 +0530

    Variable Practice Question 8

---
+++ Variable Practice Question 8/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int input = 8;
+        System.out.println(input + " x " + 1 + " = " + input);
+        System.out.println(input + " x " + 2 + " = " + input * 2);
+        System.out.println(input + " x " + 3 + " = " + input * 3);
+        System.out.println(input + " x " + 4 + " = " + input * 4);
+        System.out.println(input + " x " + 5 + " = " + input * 5);
+        System.out.println(input + " x " + 6 + " = " + input * 6);
+        System.out.println(input + " x " + 7 + " = " + input * 7);
+        System.out.println(input + " x " + 8 + " = " + input * 8);
+        System.out.println(input + " x " + 9 + " = " + input * 9);
+        System.out.println(input + " x " + 10 + " = " + input * 10);
+    }
+}
