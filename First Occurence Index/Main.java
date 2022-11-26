fe7bb97abd8276963afcb258565c2ce3053d8197
auspscamit <amitmadanbkn@gmail.com>
2022-11-27 00:29:33 +0530

    First Occurence Index

---
+++ First Occurence Index/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int t = sc.nextInt();
+
+        System.out.print(firstIndex(arr, t));
+    }
+
+    static int firstIndex(int[] arr, int t) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == t) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
